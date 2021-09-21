fd43982fa8d60067ca34a5a7a253747d84eb216e
hanchothar <henrycho626@gmail.com>
2021-09-21 15:24:57 +0630

    swiper finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,14 @@ nav__link.forEach( e => e.addEventListener('click', function(){
 
 // swiper
 var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 });
